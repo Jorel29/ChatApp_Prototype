6d05591006039877a935a9d8ecf0d4c19c651505
Jorel Huerto <jjhue29@gmail.com>
2023-11-25 20:11:12 -0800

    add hostname, ip, sport, dport

--- src/server.py
+++ src/server.py
@@ -3,3 +3,13 @@ import socket
 import logging
 
 logging.basicConfig(filename='server_log.log', level=logging.DEBUG)
+
+#get hostname and ip and set ports
+hostname = socket.gethostname()
+hostip = socket.gethostbyname(hostname)
+sport = 8080
+dport = 8082
+
+
+#create and bind listening socket
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
