38fc737098209fe3cfac8be5958cfecc1d13aea8
Jorel Huerto <jjhue29@gmail.com>
2023-11-25 20:34:09 -0800

    add basic listening thread for server

--- src/server.py
+++ src/server.py
@@ -10,6 +10,28 @@ hostip = socket.gethostbyname(hostname)
 sport = 8080
 dport = 8082
 
+clients = []
 
-#create and bind listening socket
+#create and bind listening socket for clients to send to
 sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+sock.bind((hostip, sport))
+
+logging.info(f'I\'m a signaling server')
+
+#listen for client connections
+#note: input is not fully sanitized
+#waits for <clientip>:<serverip> message format
+def listen():
+    try:
+        data = sock.recv(1024).decode(encoding='utf-8', errors='strict')
+    except:
+        logging.warning(f'Decode error from incoming message from a client')
+        return
+    
+    clientip, serverip = data.split(':')
+    logging.info(f'\rIncoming signal from client {clientip}(source) to {serverip}(dest)')
+    clients.append(clientip)
+
+#create a listening thread
+listener = threading.Thread(target=listen, daemon=True)
+listener.start()
